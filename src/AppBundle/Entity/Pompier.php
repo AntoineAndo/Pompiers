2b37fc7bb82f594090c2966670b201c1c98c6e85
AntoineAndo <antoine.ando@gmail.com>
2016-02-25 14:56:04 +0100

    ok

---
+++ src/AppBundle/Entity/Pompier.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Pompier
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Pompier
+{
+/*
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+*/
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    protected $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    protected $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=255)
+     */
+    protected $adresse;
+
+    /**
+     *
+     * @ORM\OneToOne(targetEntity="Calendrier", mappedBy="idPompier",cascade={"remove"})
+     */
+    protected $calendrier;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Pompier
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Pompier
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return Pompier
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set calendrier
+     *
+     * @param Calendrier $calendrier
+     *
+     * @return Pompier
+     */
+    public function setCalendrier($calendrier)
+    {
+        $this->calendrier = $calendrier;
+
+        return $this;
+    }
+
+    /**
+     * Get calendrier
+     *
+     * @return string
+     */
+    public function getCalendrier()
+    {
+        return $this->calendrier;
+    }
+
+    public function __toString(){
+        $pompier = '';
+        try{
+            $pompier = $this->getPrenom() . " " . $this->getNom();
+        }catch(Exception $e){
+        }
+        return $pompier;
+    }
+}
+
